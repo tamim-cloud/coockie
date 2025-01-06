41bd72f0304c82a3b31df99cc65a5ee8abc8e2be
tamim-cloud <tamimhossainforcoding01@gmail.com>
2025-01-07 04:02:11 +0600

    false debugShowCheckedModeBanner

--- coockie/lib/coockie_app.dart
+++ coockie/lib/coockie_app.dart
@@ -7,7 +7,8 @@ class CoockieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
